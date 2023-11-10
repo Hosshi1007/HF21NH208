69790f9e4c4fa98b985a6c1263d47bb5eaed313b
土反ロ <148731623+sakaguchikazuya@users.noreply.github.com>
2023-11-10 12:06:03 +0900

    beanフォルダ作成、DB操作用Bean追加

---
+++ HF21/WEB-INF/Beans/DBinsert_Beans.java
@@ -0,0 +1,89 @@
+package Beans;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBinsert_Beans implements Serializable{
+	//DB接続定義
+	/* DB:URL*/
+	final String URL = "jdbc:mysql://localhost/nhs20733db?useUnicode=true&characterEncoding=UTF-8";
+
+	/* DB:ユーザ*/
+	final String USER = "root";
+
+	/* DB:パスワード*/
+	final String PASS = "root";
+
+	/* DB:ドライバ*/
+	final String DRIVER = "org.gjt.mm.mysql.Driver";
+
+	Connection con = null;
+	Statement stmt = null;
+
+	//プロパティ定義
+	String Conditions = "";
+	String table_name = "";
+	String fields = "*";
+
+	//コンストラクタ定義
+	public DBinsert_Beans() {
+		super();
+	}
+
+	//DBアクセス
+	public void DBinsert() {
+		try {
+			//MySQLローディング
+			Class.forName(DRIVER);
+			//MySQLへの接続
+			con = DriverManager.getConnection(URL, USER, PASS);
+			//SQL格納
+			stmt = con.createStatement();
+			//SQL実行
+			Statement stmt = con.createStatement();
+			StringBuffer query = new StringBuffer();
+			query.append("insert into ");
+			query.append(table_name);
+			query.append(" (");
+			query.append(fields);
+			query.append(") values (");
+			query.append(Conditions);
+			query.append(")");
+
+			System.out.println(query.toString());
+
+			stmt.executeUpdate(query.toString());
+
+			//切断
+			stmt.close();
+			con.close();
+
+		}
+		//例外処理
+		catch(SQLException ex) {
+			ex.printStackTrace();
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	//アクセサメソッド(setter)
+	public void setTable(String inTable) {
+		//テーブル名をそのまま入力(例:"t_user")
+		table_name = inTable;
+	}
+	public void setFields(String inFields) {
+		//フィールド名をカンマ(,)区切りで入力(例:"user_name,user_password")
+		fields = inFields;
+	}
+	public void setstatus(String instatus) {
+		//登録内容をカンマ区切りで入力(例:"sakaguchi,pass12345")
+		Conditions = instatus;
+	}
+
+	//アクセサメソッド(getter)
+}
---
+++ HF21/WEB-INF/Beans/DBupdate_bean.java
@@ -0,0 +1,90 @@
+package Beans;
+
+import java.io.Serializable;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+
+public class DBupdate_bean implements Serializable{
+	//DB接続定義
+	/* DB:URL*/
+	final String URL = "jdbc:mysql://localhost/nhs20733db?useUnicode=true&characterEncoding=UTF-8";
+
+	/* DB:ユーザ*/
+	final String USER = "root";
+
+	/* DB:パスワード*/
+	final String PASS = "root";
+
+	/* DB:ドライバ*/
+	final String DRIVER = "org.gjt.mm.mysql.Driver";
+
+	Connection con = null;
+	Statement stmt = null;
+
+	//プロパティ定義
+	String Conditions = "";
+	String table_name = "";
+	String status = "";
+
+	//コンストラクタ定義
+	public DBupdate_bean() {
+		super();
+	}
+
+	//DBアクセス
+	public void DBupdate() {
+		try {
+			//MySQLローディング
+			Class.forName(DRIVER);
+			//MySQLへの接続
+			con = DriverManager.getConnection(URL, USER, PASS);
+			//SQL格納
+			stmt = con.createStatement();
+			//SQL実行
+			Statement stmt = con.createStatement();
+			StringBuffer query = new StringBuffer();
+			query.append("update ");
+			query.append(table_name);
+			query.append(" set ");
+			query.append(status);
+			query.append(" where ");
+			query.append(Conditions);
+
+			System.out.println(query.toString());
+
+			stmt.executeUpdate(query.toString());
+
+			//切断
+			stmt.close();
+			con.close();
+
+		}
+		//例外処理
+		catch(SQLException ex) {
+			ex.printStackTrace();
+		}
+		catch(Exception ex) {
+			ex.printStackTrace();
+		}
+	}
+
+	//アクセサメソッド(setter)
+	public void setTable(String inTable) {
+		//テーブル名をそのまま入力(例:"t_user")
+		table_name = inTable;
+	}
+	public void setStatus(String inStatus) {
+		//更新内容を「列名」=「値」の形でカンマ(,)区切りにして入力
+		//(例:"user_name = '佐藤次郎',password = 'root123'")
+		status = inStatus;
+	}
+	public void setConditions(String inConditions) {
+		//更新するレコードの主キーを指定する
+		//(例:"user_ID = '00001'")
+		Conditions = inConditions;
+	}
+
+	//アクセサメソッド(getter)
+}
